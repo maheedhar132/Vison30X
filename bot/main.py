d2a087ca803bdcc8c929710ea10c2962bb0305a7
maheedhar132 <maheedhartalluri@gmail.com>
2025-08-24 22:03:33 +0530

    commented remainders

--- bot/main.py
+++ bot/main.py
@@ -7,7 +7,7 @@ from telegram.ext import ApplicationBuilder
 from bot.handlers import setup_handlers
 from bot.scheduler import setup_jobs
 from bot.db import init_db
-from bot.reminders import setup_reminders  # if you added reminders
+#from bot.reminders import setup_reminders  # if you added reminders
 
 def main():
     load_dotenv(Path(__file__).parent.parent / ".env")
@@ -23,7 +23,7 @@ def main():
 
     setup_handlers(app)
     setup_jobs(app)
-    setup_reminders(app)
+    #setup_reminders(app)
 
     print("✅ Vison30X Bot is running...")
     app.run_polling()
